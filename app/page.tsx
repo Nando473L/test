042f7527eac5aa38f13174e61a6e2df040c64609
Nando473L <ferc3455@gmail.com>
2024-01-16 12:30:25 -0600

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,33 @@
 import Link from "next/link"
+import Head from "next/head"
+import styles from "@/styles/styles.module.css"
+import Footer from "@/components/Footer"
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ul>
-        <li>
-          <Link href="/">
-          Home
-          </Link>
-        </li>
-        <li>
-          <Link href="/page/test">
-          Tesst
-          </Link>
-        </li>
-      </ul>
-    </main>
+      <div className={styles.container}>
+        {/*<Head>
+          <title> Mi página web</title>
+          <meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+  </Head>*/}
+        {/*Navabar*/}
+        <nav className={styles.navbar}>
+        <a href="#" className={styles.navbarBrand}>Home</a>
+        <ul className={styles.navbarNav}>
+          <li className={styles.navItem}>
+            <a href="#" className={styles.navLink}>Ayuda </a>
+          </li>
+          <li className={styles.navItem}>
+            <a href="#" className={styles.navLink}>Sesion </a>
+          </li>
+        </ul>
+        </nav>
+        <div className={styles.content}>
+          <h1>Hola mundo</h1>
+        </div>
+        {/* componente Footer*/}
+        <Footer/>
+      </div>  
   )
 }
---
+++ app/page/panza/page.tsx
@@ -0,0 +1,10 @@
+export default function Test() {
+  return (
+    <div>
+      Gunter
+      <a href="https://i.ytimg.com/vi/6_3Jr25u6Oo/sddefault.jpg" target="_blank" rel="noopener noreferrer">
+        <img src="https://i.ytimg.com/vi/6_3Jr25u6Oo/sddefault.jpg" alt="Gunter Image" />
+      </a>
+    </div>
+  );
+}
